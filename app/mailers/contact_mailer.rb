12fa4d7c0970e6a4f492870b9b530cd51eb37e10
Ankit Shukla <ankitemailaccount@yahoo.in>
2015-06-09 14:36:02 +0000

    added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,14 @@
+class ContactMailer < ActionMailer::Base
+  default to: 'ankitemailaccount@yahoo.in'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from: email, subject: 'contact Form Message')
+  end
+end
+
+    
+    
